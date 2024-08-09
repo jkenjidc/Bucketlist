4012a5b452e836217c56950baea7a0b414111c8e
Justine Kenji Dela Cruz <kenjidckenji@gmail.com>
2024-08-09 11:24:35 -0400

    Initial Commit

---
+++ Bucketlist/BucketlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketlistApp.swift
+//  Bucketlist
+//
+//  Created by Kenji Dela Cruz on 8/9/24.
+//
+
+import SwiftUI
+
+@main
+struct BucketlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
